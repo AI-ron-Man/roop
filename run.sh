4ffdd9c3823c64744ad57be2d5671bfa2dd3cd5d
Chris Powers <cpowers@outlook.com>
2023-07-12 14:12:04 -0500

    Add Dockerfile.cpu

---
+++ run.sh
@@ -0,0 +1,8 @@
+docker build . -f Dockerfile.cpu -t roop-cpu
+docker run -it --rm \
+    -v ~/ML/media/:/roop/media/ \
+    -v ~/ML/models:/roop/models \
+    -v ~/ML/models/.insightface/:/root/.insightface/ \
+    -v ~/ML/models/.opennsfw2/:/root/.opennsfw2/ \
+    roop-cpu \
+    -s ./media/source.jpeg -t ./media/target.png -o ./media/output.png
